0e6062aea602464ea3ec1f6059edab9d3fbd6884
AFirooz (Home PC) <ali.emad649@gmail.com>
2024-12-29 01:20:20 -0500

    remove extra files

--- __tests__/services/encode-file.spec.ts
+++
@@ -1,7 +0,0 @@
-import { encodeFile } from "../../src/services/encode-file"
-
-describe("Encode file", () => {
-    test("encode", () => {
-        expect(encodeFile([49])).toEqual({ value: ['31000000000000000000000000000000'], padding: 30 })
-    });
-});
