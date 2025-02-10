dcb6c2a0152bf2c076497b2618d8d40132621465
AFirooz (MacOS) <ali.emad649@gmail.com>
2025-02-09 23:54:35 -0500

    WIP: making sample website

--- src/services/decode-text.ts
+++
@@ -1,4 +0,0 @@
-import { TREZOR_MAX_PAYLOAD_SIZE } from '../constants'
-import {  groupByLength } from '../utils/string-converters'
-
-export const decodeText = (text: string) => groupByLength(text, TREZOR_MAX_PAYLOAD_SIZE)
